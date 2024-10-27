9e920f299e1878bfc57c5ca077cf17e4b44bf0c0
Erick0105 <erickspot01@gmail.com>
2024-10-27 20:11:46 -0300

    att o page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useEffect, useState } from 'react';
 import CardAluno from '@/components/CardAluno/CardAluno';
 import { TipoAlunos } from '@/types';
--- src/components/CardAluno/CardAluno.tsx
+++ src/components/CardAluno/CardAluno.tsx
@@ -1,16 +1,18 @@
+import { TipoAlunos } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CardAlunoProps {
   nome: string;
   imagem: string;
 }
 
-export default function CardAluno({nome, imagem}: CardAlunoProps) {
+export default function CardAluno({nome, img, id}: TipoAlunos) {
   return (
     <div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center">
-      <Image src={imagem} alt={nome} className="w-24 h-24 rounded-full mb-4" />
+      <Image src={img} alt={nome} className="w-24 h-24 rounded-full mb-4" />
       <h2 className="text-xl font-semibold">{nome}</h2>
-      {/* <Link href={`aluno/`}></Link> */}
+      <Link href={`aluno/${id}`}></Link>
     </div>
   );
 }
