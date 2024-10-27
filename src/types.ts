5967be2d435302ed88c26a27f47ce768b99e559d
Erick0105 <erickspot01@gmail.com>
2024-10-27 19:21:42 -0300

    tipos dos dados atualizado

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@ export type TipoAlunos = {
     img: string;
 
     gs:TipoGs;
-    challenge:TipoChallenge;
+    challenge:TipoChallenge[];
     checkpoints:TipoCheckpoints;
 }
 
@@ -20,9 +20,27 @@ export type TipoGs = {
 }
 
 export type TipoChallenge = {
-
+    sprint:number;
+    python:number;
+    data:number;
+    front:number;
+    software:number;
+    java:number;
+    ia:number;
+    descricao:string;
 }
 
 export type TipoCheckpoints = {
-    
+    cp1:TipoCp[];
+    cp2:TipoCp[];
+    cp3:TipoCp[];
+    cp4:TipoCp[];
+    cp5:TipoCp[];
+}
+
+export type TipoCp = {
+    materia:string;
+    nota:number;
+    data:string;
+    feedback:string;
 }
